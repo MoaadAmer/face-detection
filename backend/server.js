69913ba8bf5217279b5deb518d747de0e035c5c4
Moaad Amer <moaad02@gmail.com>
2025-05-29 16:25:26 +0300

    create the server

---
+++ backend/server.js
@@ -0,0 +1,93 @@
+import express from "express";
+import fs from "node:fs/promises";
+
+let database;
+async function readDatabase() {
+  try {
+    database = JSON.parse(
+      await fs.readFile("./database.json", { encoding: "utf8" })
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function saveDatabase() {
+  try {
+    await fs.writeFile("./database.json", JSON.stringify(database));
+    await readDatabase();
+  } catch (err) {
+    console.error(err.message, "20");
+  }
+}
+
+await readDatabase();
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.json(database.users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = database.users.find((user) => user.id === id);
+  if (user === undefined) {
+    res.status(404).json("No such user");
+  }
+  res.json(user);
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  const validUser = database.users.some(
+    (user) => user.email === email && user.password === password
+  );
+  if (validUser) {
+    res.json("success");
+  } else {
+    res.status(400).json("invalid");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  if (name === "" || email === "" || password === "") {
+    res.status(400).json("all fields are mandatory");
+  } else if (database.users.some((user) => user.email === email)) {
+    res
+      .status(400)
+      .json("this email already registerd, please enter different one");
+  } else {
+    const newUser = {
+      id: getNextId().toString(),
+      name: name,
+      email: email,
+      password: password,
+    };
+    database.users.push(newUser);
+    await saveDatabase();
+    res.json(newUser);
+  }
+});
+
+app.put("/users/:id/image", (req, res) => {
+  const { id } = req.params;
+  let user = database.users.find((user) => user.id === id);
+  if (user === undefined) {
+    res.status(404).json("No such user");
+  }
+  user.entries++;
+  saveDatabase();
+  res.json(user);
+});
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+function getNextId() {
+  return Math.max(...database.users.map((user) => Number(user.id))) + 1;
+}
